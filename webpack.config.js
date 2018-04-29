782f53122454f861320b4ce4ff30794d00986566
ya6maDev <y5a1m5a@gmail.com>
2018-04-29 23:19:25 +0900

    新規追加

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const publidDir = path.join(__dirname, '/public');
+
+module.exports = [
+  {
+    entry: [
+      './views/index.jsx',
+    ],
+    output: {
+      path: publidDir,
+      publicPath: '/',
+      filename: 'bundle.js',
+    },
+    module: {
+      loaders: [{
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+      ],
+    },
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    devServer: {
+      hot: true,
+      historyApiFallback: true,
+      contentBase: publidDir,
+    },
+  },
+  {
+    entry: {
+      style: './stylesheets/index.scss',
+    },
+    output: {
+      path: publidDir,
+      publicPath: '/',
+      filename: 'bundle.css',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }),
+        },
+        {
+          test: /\.scss$/,
+          loader: ExtractTextPlugin.extract(
+            {
+              fallback: 'style-loader',
+              use: 'css-loader!sass-loader',
+            }),
+        },
+      ],
+    },
+    plugins: [
+      new ExtractTextPlugin('bundle.css'),
+    ],
+  },
+];
